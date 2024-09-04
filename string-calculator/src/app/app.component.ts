cf89b9aac5e722e9c8267f4c506c58d5e080d510
shubhammagar <magarshubham13@gmail.com>
2024-09-04 19:45:02 +0530

    implemented user intnerface with ngModule used ngIf

--- string-calculator/src/app/app.component.ts
+++ string-calculator/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StringCalculatorService } from './string-calculator.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'string-calculator';
+  numbers: string = '';
+  result: number | null = null;
+  errorMessage: string | null = null;
+
+  constructor(private stringCalculatorService: StringCalculatorService) {}
+
+  calculateSum() {
+    try {
+      this.result = this.stringCalculatorService.add(this.numbers);
+      this.errorMessage = null; // Clear any previous errors
+    } catch (error:any) {
+      this.errorMessage = error.message;
+      this.result = null; // Clear the result if there's an error
+    }
+  }
 }
