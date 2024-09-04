fcc5541dcb337eeef1832a098dc8051cca6cda89
shubhammagar <magarshubham13@gmail.com>
2024-09-04 18:59:28 +0530

    project creation and service creation

---
+++ string-calculator/src/app/string-calculator.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { StringCalculatorService } from './string-calculator.service';
+
+describe('StringCalculatorService', () => {
+  let service: StringCalculatorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(StringCalculatorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
